74078ed9fb1a0d06ce9a95a899e4260ca7217f85
pswoodworth <pswoodworth@gmail.com>
2015-04-22 01:29:09 -0400

    ready for junk deploy

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.all('*', function(req, res, next) {
 // app.get('/blah', routeHandler);
 
 var allContent = {
+	title: 'Menu',
+	contains: 'list',
 	content:[
 		{
 		title: 'First Thing',
@@ -40,8 +42,36 @@ var allContent = {
 	]
 };
 
+var tempContent = {
+	title: 'Menu',
+	contains: 'list',
+	content:[
+		{
+		title: 'Home',
+		contains: 'html',
+		content: '<p><b>this entire app is very rough, and only for demonstration purposes at this point. there is a lot still to be done in styling and typesetting, as well as adding in more content</b><br><br><br>This space will contain upcoming events.</p>'
+		},
+		{
+		title: 'About',
+		contains: 'list',
+		content: [
+			{
+				title: 'BSAC',
+				contains: 'html',
+				content: "<p>The Boston Student Advisory Council is a citywide body of elected student leaders representing most Boston Public High Schools. BSAC is co-administered by Youth on Board and the Boston Public Schools Office of Engagement. BSAC works to identify and address issues that matter the most to students, thereby putting young people at the center of the decisions that affect them the most. BSAC students have led organizing efforts, forged relationships with district and city-leaders, impacted policy change, and transformed school culture across the board.<br><br><b>If you are interested in becoming a BSAC member or attending a meeting please contact: bsac@bostonpublicschools.org</b><br><br> Find us on Facebook!<br>Youtube & Twitter!</p>"
+			},
+			{
+				title: 'School Discipline & Student Rights',
+				contains: 'html',
+				content: '<p>Students Have Rights! Boston and Massachusetts have some of the most progressive and student friendly school discipline policies in the U.S.. However, Boston students are still being unfairly kicked out of their schools for minor offenses with students of color, students with disabilities, and low-income students being most affected by harsh discipline. In the 2012-2013 School Year: Massachusetts students missed at least 208,605 days in the classroom due to disciplinary removal. nearly two-thirds (72%) of all these out-of-school suspensions were for “non-violent, non-criminal, non-drug’ offenses. Students of color, students with disabilities, and low-income students experienced a disproportionate share of these disciplinary removals from school. Students of color were disciplined more harshly and more often than white students for “non-violent, non-criminal, non-drug” incidents. Students with disabilities were suspended out-of-school at three times the rate of their non-disabled peers. Students receiving free or reduced lunch were disciplined at a rate almost double their enrollment Students that are taken away from their education by out of school suspension or e are more likely to fall behind in schoolwork or drop out. Harsh school discipline that remove students from the classroom contribute to the school-to-prison pipeline Source: Lawyer’s Committee for Civil Rights and Economic Justice “Not Measuring Up: The State of School Discipline in Massachusetts” 2015.</p>'
+			}
+		]
+		}
+	]
+};
+
 app.get('/content', function(req, res){
-	res.jsonp(allContent);
+	res.jsonp(tempContent);
 });
 
 app.post('/save-content', function(req, res){
